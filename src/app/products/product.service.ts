cbbded86902a72a58ae84d829f17666689c5dc8f
Juliano <julianoomc@gmail.com>
2022-05-10 10:47:03 -0300

    Reacting to selection: Data Stream

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -30,7 +30,15 @@ export class ProductService {
         category: categories.find(c => product.categoryId === c.id)?.name,
         searchKey: [product.productName]
       } as Product))),
-  )
+  );
+
+  selectedProduct$ = this.productsWithCategory$
+    .pipe(
+      map(products =>
+        products.find(product => product.id === 5)
+      ),
+      tap(product => console.log('selectedProduct', product))
+    );
   
   constructor(private http: HttpClient,
               private productCategoryService: ProductCategoryService) { }
